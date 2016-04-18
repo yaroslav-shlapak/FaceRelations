5e344b5d5da2aa0af3e07c0f2eee9718097538d1
yaroslav <yaroslav.shlapak@gmail.com>
2016-04-19 01:41:34 +0300

    Tried to show user name

--- app/src/main/java/com/voidgreen/friendsrelations/MainActivity.java
+++ app/src/main/java/com/voidgreen/friendsrelations/MainActivity.java
@@ -1,5 +1,6 @@
 package com.voidgreen.friendsrelations;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -10,7 +11,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.Profile;
+import com.voidgreen.facerelations.*;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -18,21 +25,86 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.voidgreen.facerelations.R;
+import com.facebook.login.widget.LoginButton;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
     private DrawerLayout drawerLayout;
     private View content;
+    @Bind(R.id.login_button)LoginButton loginButton;
+    private Button postImageButton;
+    private Button updateStatusButton;
+
+    private TextView userName;
+    CallbackManager callbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+        callbackManager = CallbackManager.Factory.create();
+/*
+
+        userName = (TextView) findViewById(R.id.user_name);
+
+
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+
+
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        // App code
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+
+                    }
+                });*/
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(MainActivity.this, "onSuccess", Toast.LENGTH_SHORT).show();
+                        Profile profile = Profile.getCurrentProfile();
+                       // userName.setText(profile.getFirstName());
+                        Toast.makeText(MainActivity.this, profile.getFirstName(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+
 
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,7 +119,8 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
         view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
                 Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
                 menuItem.setChecked(true);
                 drawerLayout.closeDrawers();
@@ -55,27 +128,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-        CallbackManager callbackManager = CallbackManager.Factory.create();
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // App code
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                    }
-                });
     }
 
     @Override
@@ -88,5 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
 }
